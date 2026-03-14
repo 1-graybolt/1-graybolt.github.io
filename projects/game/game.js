f92bdc7cfdf7fb806b368906af8dde85c51191af
Edom Bedane <tullubizuwork@gmail.com>
2026-03-13 20:47:48 -0400

    fixing

--- projects/game/game.js
+++ projects/game/game.js
@@ -22,16 +22,16 @@ function check_time() {
 function Bedroom() {
     clear();
     print("\nYou wake up in your bedroom");
-    print("this wasn't how you remember going to sleep...");
-    print("Bang!")
-    print("every alarm goes off, you only have 15 minutes to evacuate hurry.");
+    print("\nthis wasn't how you remember going to sleep...");
+    print("\nBang!");
+    print("\nevery alarm goes off, you only have 15 minutes to evacuate hurry.");
     print("\nWhere do you want to go next to escape? Say one of these choices:" +
         "\n\tHallway");
     
     function processInput(input){
         if (input.toLowerCase() === "Hallway") {
             Hallway();
-            print("Everything is super dark you turn on the lights and realized there is 4 ways to go.")
+            print("\nEverything is super dark you turn on the lights and realized there is 4 ways to go.")
             print("\nWhere do you want to go next to escape? Say one of these choices:" +
         "\n\tbedroom" + "\n\tkitchen" + "\n\tlivingroom" + "\n\trestroom" )
         } else {
@@ -42,9 +42,9 @@ function Bedroom() {
     waitForInput(processInput);
 }
 
-function locationB() {
+function Hallway() {
     clear();
-    print("\nYou are in location B!");
+    print("\nYou are in the hallway!");
     print("\nWhere do you want to go next? Say one of these choices:" +
         "\n\tbedroom");
     
@@ -62,19 +62,11 @@ function locationB() {
 //finally, make sure you customize this to tell it what should happen at the
 //very start. For this simple example, any input will bring you
 //to locationA
-function start() {
-    clear();
-    print("Welcome to my game!");
-    print("Type 'start' and press Enter to begin.");
+function start(){
+    print("Welcome to my game! Press any key to start");
 
-    function processInput(input) {
-        if (input.toLowerCase() === "start") {
-            bedroom();
-        } else {
-            print("You must type 'start' to begin!");
-            waitThenCall(start, 1000);
-        }
+    function processInput(input){
+            Bedroom();
     }
     waitForInput(processInput);
 }
-start();
