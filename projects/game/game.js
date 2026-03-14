6f27184dc816f3f3eefb5f1a4b4d77667006a7df
Edom Bedane <tullubizuwork@gmail.com>
2026-03-14 11:34:21 -0400

    changeing bedroom to lowercase

--- projects/game/game.js
+++ projects/game/game.js
@@ -18,9 +18,9 @@ function check_time() {
 //If you need, add any "helper" functions here
 
 //Make one function for each location
-function Bedroom() {
+function bedroom() {
     clear();
-    print("\nYou wake up in your Bedroom");
+    print("\nYou wake up in your Bbedroom");
     print("\nthis wasn't where you remember going to sleep...");
     print("\nBang!");
     print("\nevery alarm goes off, you only have 15 minutes to evacuate hurry.");
@@ -38,7 +38,7 @@ function processInput(input){
         livingroom();
     } else {
         stayHere();
-        waitThenCall(Bedroom);
+        waitThenCall(bedroom);
     }
 }
 waitForInput(processInput);
@@ -48,11 +48,11 @@ function hallway() {
     clear();
     print("\nEverything is super dark you turn on the lights and realized there is 4 ways to go.")
             print("\nWhere do you want to go next to escape? Say one of these choices:" +
-        "\n\tBedroom" + "\n\tkitchen" + "\n\tlivingroom" + "\n\trestroom" );
+        "\n\tbedroom" + "\n\tkitchen" + "\n\tlivingroom" + "\n\trestroom" );
     
     function processInput(input){
-        if (input.toLowerCase() === "Bedroom") {
-            Bedroom();
+        if (input.toLowerCase() === "bedroom") {
+            bedroom();
         } else if (input.toLowerCase() === "kitchen") {
             kitchen();
         } else if (input.toLowerCase() === "livingroom") {
@@ -93,7 +93,7 @@ function start(){
     print("Welcome to my game! Press any key to start");
 
     function processInput(input){
-            Bedroom();
+            bedroom();
     }
     waitForInput(processInput);
 }
