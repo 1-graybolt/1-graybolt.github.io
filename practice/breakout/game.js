c4d8e45e56ed301336ebc5b239ae4b6f024b6fd0
Edom Bedane <tullubizuwork@gmail.com>
2026-03-19 14:37:08 -0400

    done with this assignment.

--- practice/breakout/game.js
+++ practice/breakout/game.js
@@ -1,40 +1,59 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-const ballRadius = 10;
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-let dx = 2;
-let dy = -2;
-
-function drawBall() {
-  ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
-}
-
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBall();
-
-  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-    dx = -dx;
-  }
-  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-    dy = -dy;
-  }
-
-  x += dx;
-  y += dy;
-}
-
-function startGame() {
-  setInterval(draw, 10);
-}
-
-const runButton = document.getElementById("runButton");
-runButton.addEventListener("click", () => {
-  startGame();
-  runButton.disabled = true;
-});
+function drawBall(){
+				ctx.beginPath();
+				ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+				ctx.fillStyle = "#0095DD";
+				ctx.fill();
+				ctx.closePath();
+			}
+			function drawPaddle(){
+				ctx.beginPath();
+				ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+				ctx.fillStyle = "#0095DD";
+				ctx.fill();
+				ctx.closePath();
+			}
+			function draw(){
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				drawBall();
+				drawPaddle();
+				if (x + dx > canvas.width - ballRadius || x + dx < ballRadius){
+					dx = -dx;
+				}
+				if (y + dy > canvas.height - ballRadius || y + dy < ballRadius){
+					dy = -dy;
+				}
+				x += dx;
+				y += dy;
+				if (rightPressed){
+					paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+				}
+				else if (leftPressed){
+					paddleX = Math.max(paddleX - 7, 0);
+				}
+			}
+			function startGame(){
+				document.addEventListener("keydown", keyDownHandler);
+				document.addEventListener("keyup", keyUpHandler);
+				function keyDownHandler(e){
+					if (e.key == "Right" || e.key == "ArrowRight"){
+						rightPressed = true;
+					}
+					else if (e.key == "Left" || e.key == "ArrowLeft"){
+						leftPressed = true;
+					}
+				}
+				function keyUpHandler(e){
+					if (e.key == "Right" || e.key == "ArrowRight"){
+						rightPressed = false;
+					}
+					else if (e.key == "Left" || e.key == "ArrowLeft"){
+						leftPressed = false;
+					}
+				}
+				setInterval(draw, 10);
+			}
+			const runButton = document.getElementById("runButton");
+			runButton.addEventListener("click", () => {
+				startGame();
+				runButton.disabled = true;
+			});
