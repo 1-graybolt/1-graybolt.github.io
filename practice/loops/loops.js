a6396cc338f78a619d156e4cdedd6113d04c66f7
Edom Bedane <tullubizuwork@gmail.com>
2026-03-26 16:04:58 -0400

    finally Its working

--- practice/loops/loops.js
+++ practice/loops/loops.js
@@ -22,6 +22,7 @@ for(let x = 25; x < 800; x += 100){
 
 canvas = document.getElementById("canvas3");
 ctx = canvas.getContext("2d");
+
 //fill in code for canvas3 her
 for(let y = 0; y < 800; y += 100){
 for(let x = 0; x < 800; x += 100){
@@ -33,6 +34,17 @@ for(let x = 0; x < 800; x += 100){
 
 canvas = document.getElementById("canvas4");
 ctx = canvas.getContext("2d");
+
 //fill in code for canvas4 here
+for(let y=0;y<=600;y+=200){
+for(let x=0;x<=600;x+=200){
+ctx.fillRect(x,y,100,100);
+}
+}
+for(let y=100;y<=700;y+=200){
+for(let x=100;x<=800;x+=200){
+ctx.fillRect(x,y,100,100);
+}
+}
 
 
